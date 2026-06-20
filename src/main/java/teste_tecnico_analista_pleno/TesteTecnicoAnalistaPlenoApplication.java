e0b366c7268aed7bba689851e8858c0b7d3db188
mourao-dev <beatrizamourao@gmail.com>
2026-06-20 14:03:00 -0300

    chore: initial commit, java version setup and h2 config

---
+++ src/main/java/teste_tecnico_analista_pleno/TesteTecnicoAnalistaPlenoApplication.java
@@ -0,0 +1,13 @@
+package teste_tecnico_analista_pleno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTecnicoAnalistaPlenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTecnicoAnalistaPlenoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teste_tecnico_analista_pleno/TesteTecnicoAnalistaPlenoApplicationTests.java
@@ -0,0 +1,13 @@
+package teste_tecnico_analista_pleno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteTecnicoAnalistaPlenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
